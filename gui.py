aed49f54197f02c4e9472c02db480a884bb8b6cd
GCooper <giles.cooper@teledyne.com>
2025-02-20 14:17:21 -0500

    gui creation

--- gui.py
+++ gui.py
@@ -1,3 +1,26 @@
 from obj import Recipe, RecipeStep, Settings
+from PyQt6 import QtWidgets, uic
+import sys
+
+from ui import Ui_MainWindow
+from step import Ui_Form
 
 print(Settings.read().file_format())
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
+
+class StepForm(QtWidgets.QFormLayout, Ui_Form):
+    def __init__(self, *args, obj=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+app.exec()
